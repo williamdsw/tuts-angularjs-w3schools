dedef86db9bd8c05cb6aa1ba7ecfb30eeee642c1
William Santos <williamdsw@outlook.com>
2020-06-13 12:20:34 -0300

    006 - Controller

---
+++ js/06-controller/controller.js
@@ -0,0 +1,14 @@
+
+
+// adds a controller
+app.controller ('myController', function ($scope) {
+    $scope.teams = [
+        { position: 1, name: 'Liverpool', country: 'England' },
+        { position: 2, name: 'Flamengo', country: 'Brazil' },
+        { position: 3, name: 'Monterrey', country: 'Mexico' },
+        { position: 4, name: 'Al-Hilal', country: 'Saudi Arabia' },
+        { position: 5, name: 'Espérance Sportive de Tunis ', country: 'Tunisia' },
+        { position: 6, name: 'Al-Saad', country: 'Qatar' },
+        { position: 7, name: 'Hienghène Sport', country: 'New Caledonia ' }
+    ];
+});
