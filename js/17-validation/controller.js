08fb81ec61871be270a9ad285361faf5ec239dd8
williamdsw <williamdsw@outlook.com>
2020-06-15 22:10:54 -0300

    017 - Validations

---
+++ js/17-validation/controller.js
@@ -0,0 +1,11 @@
+
+// adds a controller
+app.controller('formController', function ($scope) {
+    name = '';
+    age = '';
+    country = '';
+    haveCar = false;
+    maritalStatus = '';
+
+    $scope.countries = ['Brazil', 'US', 'Canada', 'France', 'Italy'];
+});
